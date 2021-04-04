ccdeb7a32528832b7232df1369acbba568afe438
JackJuni <77576339+JackJuni@users.noreply.github.com>
2021-04-04 15:11:33 -0300

    Adicionando funções e outras funcionalidades.

--- Funções/__init__.py
+++ Funções/__init__.py
@@ -1,3 +1,6 @@
+import sqlite3
+
+
 class Roupa:
 
     def __init__(self, nome, preco, codigo_de_barra, quantidade):
@@ -7,8 +10,33 @@ class Roupa:
         self.quantidade = quantidade
 
     def cadastrar(self):
-        import json
-        with open('documentos.json', 'a') as arquivo:
-            json.dump(f'{self.nome};{self.preco};{self.barra}\n', arquivo)
-            print(f'Roupa {self.nome} adicionada com sucesso')
-            arquivo.close()
+        """ Cadastra Novas peças de roupas no banco de dados
+        """
+        con = sqlite3.connect("dados.db")
+        c = con.cursor()
+
+        c.execute("INSERT INTO produtos VALUES (?, ?, ?, ?)", (self.barra, self.nome, self.preco, self.quantidade))
+
+        con.commit()
+        con.close()
+
+
+def remocao(barra, quantidade):
+    """ Função para remover peças de roupa
+    :param barra: Código de barras
+    :param quantidade: quantidade a ser retirada
+    """
+    verlista()
+    con = sqlite3.connect("dados.db")
+    c = con.cursor()
+    c.execute("UPDATE produtos SET quantidade = ? WHERE codigo_de_barra = ?", (quantidade, barra))
+
+
+def verlista():
+    con = sqlite3.connect("dados.db")
+    c = con.cursor()
+
+    for lista in c.execute('SELECT * FROM produtos'):
+        print(lista)
+
+    con.close()
--- Programa.py
+++ Programa.py
@@ -4,19 +4,31 @@ while True:
     opcao = int(input('[0] Cadastrar novas roupas\n'
                       '[1] Retirar peças de roupas\n'
                       '[2] Parar o programa\n'
-                      '[3] Ver roupas'
+                      '[3] Ver roupas\n'
                       'Digite uma opção: '))
 
     if opcao == 0:
+        # Cadastrar novas roupas
         from Funções import Roupa
+
         nome = str(input('Digite um nome de roupa: '))
-        codigo = str(input('Digite o códgio de barras: '))
-        preco = float(input('Digite o preço: '))
+        codigo = str(input('Digite o código de barras: '))
+        preco = float(input('Digite o preço: R$'))
         quantidade = int(input('Digite a quantidade: '))
+
         roupa = Roupa(nome, preco, codigo, quantidade)
         roupa.cadastrar()
+        print(f'Roupa {nome} cadastrada com sucesso!')
+
+    elif opcao == 1:
+        barra_roupa = int(input('Qual o código de barras da roupa você gostaria de retirar? '))
+        quant = int(input('Qual a quantidade? '))
 
     elif opcao == 2:
         break
+
+    elif opcao == 3:
+        from Funções import verlista
+        verlista()
 
 print('Fim do programa')
