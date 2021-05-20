20220ab65a8cf86ef37c30c1de330aa325f6e1b8
JackJuni <77576339+JackJuni@users.noreply.github.com>
2021-05-20 18:07:32 -0300

    Leia a descrição
    Mudando a função remocaoeadicao
    
    Função adição funcionando perfeitamente junto com mudanças no adicionarhistorico, tendo como fim resumir e especificar o que foi mudado
    
    Começando a fazer a funcionalidade excluir → Para excluir algum produto do banco de dados
    
    Exclui o código antigo

--- Programa.py
+++ Programa.py
@@ -1,185 +1,18 @@
 import Funções
 from PyQt5 import QtWidgets, QtGui
-from PyQt5.QtGui import QIntValidator
+from PyQt5.QtGui import QIntValidator, QDoubleValidator
 import uic2
-# from Funções import *  -> Opção <-
 
-# while True:
-#
-#     opcao = int(input('[0] Cadastrar novas roupas\n'
-#                       '[1] Compras (Retirar peças de roupas)\n'
-#                       '[2] Parar o programa\n'
-#                       '[3] Ver o estoque\n'
-#                       '[4] Adicionar peças de roupas\n'
-#                       '[5] Ver o histórico de remoção de roupas\n'
-#                       'Digite uma opção: '))
-#
-#     if opcao == 0:
-#         # Cadastrar novas roupas
-#
-#         nome = str(input('Digite um nome de roupa: '))
-#         cdb = str(input('Digite o código de barras: '))
-#         valor = float(input('Digite o preço: R$'))
-#
-#         # As bermudas/shorts tem uma forma diferente de tamanhos
-#         op = int(input('Digite 1 se a peça é uma bermuda/shorts, se não for digite 0: '))
-#
-#         if op == 0:
-#
-#             q_u = int(input('Digite a quantidade para tamanhos Únicos(U): (0 Se nenhum) '))
-#             q_p = int(input('Digite a quantidade para tamanhos P: (0 Se nenhum) '))
-#             q_m = int(input('Digite a quantidade para tamanhos M: (0 Se nenhum) '))
-#             q_g = int(input('Digite a quantidade para tamanhos G: (0 Se nenhum) '))
-#             q_gg = int(input('Digite a quantidade para tamanhos GG: (0 Se nenhum) '))
-#             Funções.adicionarhistorico(cdb, nome,
-#                                        f'{q_u} Tamanhos U,{q_p} Tamanhos P, {q_m} Tamanhos M, {q_g} Tamanhos G, '
-#                                        f'{q_gg} Tamanhos GG', 'Novo Cadastro', 0)
-#             Funções.cadastrar(cdb, nome, valor, (q_u + q_p + q_m + q_g + q_gg), q_u, q_p, q_m, q_g, q_gg, op)
-#
-#         else:
-#             q_36 = int(input('Digite a quantidade para tamanhos 36: (0 Se nenhum) '))
-#             q_38 = int(input('Digite a quantidade para tamanhos 38: (0 Se nenhum) '))
-#             q_40 = int(input('Digite a quantidade para tamanhos 40: (0 Se nenhum) '))
-#             q_42 = int(input('Digite a quantidade para tamanhos 42: (0 Se nenhum) '))
-#             q_44 = int(input('Digite a quantidade para tamanhos 44: (0 Se nenhum) '))
-#             q_46 = int(input('Digite a quantidade para tamanhos 46: (0 Se nenhum) '))
-#             Funções.adicionarhistorico(cdb, nome, f'{q_36} Tamanhos 36,{q_38} Tamanhos 38, {q_40} Tamanhos 40,'
-#                                                   f' {q_42} Tamanhos 42, {q_44} Tamanhos 44, {q_46} Tamanhos 46',
-#                                        'Novo Cadastro', 0)
-#             Funções.cadastrar(cdb, nome, valor, (q_36 + q_38 + q_40 + q_42 + q_44 + q_46), q_36, q_38, q_40, q_42,
-#             q_44, op, q_46)
-#
-#         print(f'\033[0;32mRoupa {nome} cadastrada com sucesso!\033[m')
-#
-#     elif opcao == 1:
-#         # Retirar peças de roupas
-#
-#         # Validação de código de barras
-#         while True:
-#             # Lista de roupas
-#             Funções.verlista()
-#
-#             cdb = int(input('Digite o código de barras: '))
-#             barra_roupa = Funções.validacao_cdb(cdb)
-#             if barra_roupa:
-#                 break
-#             else:
-#                 print('\033[0;31m -> Código de barras inválido. Digite um código de barras válido. <- \033[m')
-#
-#         # Troca dos tamanhos para os valores em que estão ocupados na lista para a validação
-#         # As variaveis de tam2 recebem a posição da lista
-#
-#         # Para saber se a tal peça é Short/Bermuda
-#         op = Funções.pegarvalor(cdb, op=1)
-#
-#         if op[0] == 0:
-#
-#             while True:
-#                 tam = str(input('Qual o tamanho que você está retirando? (U, P, M, G, GG) ')).strip().upper()
-#
-#                 tam2 = tam
-#
-#                 qt = int(input('Qual a quantidade de peças que deseja retirar? '))
-#
-#                 x = Funções.pegarvalor(cdb, nome=1, valor=1)
-#
-#                 tam = Funções.transformatam(tam)
-#
-#                 val = Funções.validacao_tr(cdb, tam, qt)
-#
-#                 # Se os dados inseridos puderem ser executados ele vai dar break
-#                 if val:
-#                     break
-#                 else:
-#                     print('\033[0;31m -> Os valores inseridos estão incorretos. Por favor verifique os dados'
-#                           ' inseridos <-\033[m')
-#                     Funções.verlista()
-#
-#         else:
-#             while True:
-#                 tam = int(input('Qual o tamanho que você está retirando? (36, 38, 40, 42, 44, 46) '))
-#
-#                 tam2 = tam
-#
-#                 qt = int(input('Qual a quantidade de peças que deseja retirar? '))
-#
-#                 x = Funções.pegarvalor(cdb, nome=1, valor=1)
-#
-#                 tam = Funções.transformatam(tam, op)
-#
-#                 val = Funções.validacao_tr(cdb, tam, qt)
-#
-#                 # Se os dados inseridos puderem ser executados ele vai dar break
-#                 if val:
-#                     break
-#                 else:
-#                     print('\033[0;31m -> Os valores inseridos estão incorretos. Por favor verifique os dados'
-#                           ' inseridos <-\033[m')
-#                     Funções.verlista()
-#
-#         Funções.adicionarhistorico(cdb, x[0], f'{qt} Tamanhos {tam2}', 'Comprado', valor=x[1] * qt)
-#
-#         q_t = Funções.pegarvalor(cdb, q_t=1)
-#
-#         Funções.remocaoeadicao(cdb, tam, val-qt, q_t[0]-qt)
-#         print('Peça de roupa retirado com sucesso')
-#
-#     elif opcao == 2:
-#         # Parar o programa
-#         break
-#
-#     elif opcao == 4:
-#         # Adicionar peças de roupas
-#
-#         # Validação de código de barras (Para ver se o código de barras digitado existe)
-#         while True:
-#             Funções.verlista()
-#             cdb = int(input('Digite o código de barra da roupa: '))
-#             val = Funções.validacao_cdb(cdb)
-#             # Se o val for True quer dizer que já existe um código de barras desse sendo usado
-#             if val:
-#                 break
-#             else:
-#                 print('\033[0;31m -> Este código de barras não existe. Por favor digite um código de barras'
-#                       ' válido <-\033[m')
-#
-#         op = Funções.pegarvalor(cdb, op=1)
-#
-#         if op[0] == 0:
-#             tam = str(input('Qual o tamanho que você está adicionando? (U, P, M, G, GG) ')).strip().upper()
-#         else:
-#             tam = int(input('Qual o tamanho que você está adicionando? (36, 38, 40, 42, 44, 46) '))
-#
-#         tam2 = tam
-#
-#         qt = int(input('Deseja adicionar quantas unidades? '))
-#
-#         x = Funções.pegarvalor(cdb, nome=1)
-#
-#         tam = Funções.transformatam(tam, op)
-#
-#         #  A validação vai servir para puxar quantas roupas do tamanho x tem, para assim eu poder atualizar no banco -
-#         # de dadosqt
-#         # qt -> quantidade
-#         qt2 = Funções.validacao_tr(cdb, tam, op=2)
-#
-#         # q_t é a quantidade total
-#         q_t = Funções.pegarvalor(cdb, q_t=1)
-#
-#         Funções.remocaoeadicao(cdb, tam, qt+qt2, q_t[0]+qt)
-#
-#         Funções.adicionarhistorico(cdb, x[0], f'{qt} Tamanhos {tam2}', 'Adicionada')
-#
-#         print('Peças de roupas adicionado com sucesso')
-#
-#     elif opcao == 6:
-#         Funções.criatabela()
-#
-#     else:
-#         # Usuário não digitou um valor válido
-#         print('\033[0;31m -> Digite um valor válido <- \033[m')
-#
-# print('Fim do programa')
+
+def excluir():
+    try:
+        int(pro.cdb_add.text())
+        int(pro.cdb.text())
+    except ValueError:
+        QtWidgets.QMessageBox.about(pro, 'Erro', 'Insira um código de barras primeiro!')
+    else:
+        pass
+        msg = QtWidgets.QMessageBox()
 
 
 def habilitar(op):
@@ -293,7 +126,7 @@ def editar():
 
 def atualizar():
     """ Função para atualizar dados no banco de dados
-    Os dados editáveis são os -> Descrição, Valor, e Todos os xtamanhos
+    Os dados editáveis são os -> Descrição, Valor, Quantidade total e Todos os xtamanhos
     Os dados não editáveis são os -> Código de barras e Tipo de Tamanho"""
     cdb = int(pro.cdb.text())
     descricao = pro.descricao.text()
@@ -309,38 +142,48 @@ def atualizar():
         q_6 = 0
     q_t = q_1 + q_2 + q_3 + q_4 + q_5 + q_6
     op = Funções.pegarvalor(cdb, op=1)
-    print(op)
     # ou -> op = pro.tipotamanho.currentIndex()
     antigo = Funções.pegarvalor(cdb, todos=1)
     if antigo[-1] == 1:
         del antigo[3:8]
         novo = [descricao, valor, q_t, q_1, q_2, q_3, q_4, q_5, q_6, op[0]]
+        dic = ['Descrição', 'Valor', 'Quantidade total', 'Tamanhos 36', 'Tamanhos 38', 'Tamanhos 40', 'Tamanhos 42',
+               'Tamanhos 44', 'Tamanhos 46']
     else:
+        # op = 0
+        # ['Vestido Drills ', 49.9, 16, 3, 3, 3, 2, 5, 0, 0, 0, 0, 0, 0, 0]
+        # 8:14
+        # ['Vestido Drills ', 49.9, 16, 3, 3, 3, 2, 5, 0]
         del antigo[8:14]
         novo = [descricao, valor, q_t, q_1, q_2, q_3, q_4, q_5, op[0]]
-    print(novo)
-    print(antigo)
-    cont = []
-    y = {'Descrição': 5, 'Valor': 0}
-    print('y aqui ->', y['Descrição'])
+        dic = ['Descrição', 'Valor', 'Quantidade total', 'Tamanhos U', 'Tamanhos P', 'Tamanhos M', 'Tamanhos G',
+               'Tamanhos GG']
+
+    # situacao vai ser o valor passado ao adicionarhistorico() e nele irá conter o dic (Para especificar qual o objeto
+    # está sendo mudado) e os valores antigos para os novos
+    situacao = []
+    # Detecção de mudanças
     for x in range(0, 9):
         if novo[x] != antigo[x]:
-            if x == 0:
-                cont.append(f'Descrição mudou de {antigo[x]} para {novo[x]}. ')
-            print('diferente')
-            cont.append((antigo[x], '->', novo[x]))
-        print(x)
-    print(cont)
-    # Funções.remocaoeadicao(cdb, op, q_t, q_1, q_2, q_3, q_4, q_5, q_6, descricao, valor)
-    # if op[0] == 0:
-    #     Funções.adicionarhistorico(cdb, descricao,
-    #                                f'{q_1} Tamanhos U, {q_2} Tamanhos P, {q_3} Tamanhos M, {q_4} Tamanhos G, '
-    #                                f'{q_5} Tamanhos GG', 'Mudança', 0)
-    # else:
-    #     Funções.adicionarhistorico(cdb, descricao,
-    #                                f'{q_1} Tamanhos 36, {q_2} Tamanhos 38, {q_3} Tamanhos 40, {q_4} Tamanhos 42, '
-    #                                f'{q_5} Tamanhos 44, {q_6} Tamanhos 46', 'Mudança', 0)
-    QtWidgets.QMessageBox.about(pro, 'Sucesso', 'Peça editada com sucesso')
+            situacao.append(f'{dic[x]} mudou de {antigo[x]} para {novo[x]}')
+
+    # Irá criar a frase para o adicionarhistorico()
+    frase = ''
+    for c in range(0, len(situacao)):
+        if c == 0:
+            frase = situacao[c]
+        else:
+            frase = frase + ' , ' + situacao[c]
+
+    if not situacao:
+        QtWidgets.QMessageBox.about(pro, 'Erro', 'Não houve mudança, tente novamente.')
+    else:
+        Funções.remocaoeadicao(cdb, op, q_t, q_1, q_2, q_3, q_4, q_5, q_6, descricao, valor)
+        if op[0] == 0:
+            Funções.adicionarhistorico(cdb, descricao, frase, 'Mudança')
+        else:
+            Funções.adicionarhistorico(cdb, descricao, frase, 'Mudança')
+        QtWidgets.QMessageBox.about(pro, 'Sucesso', 'Peça editada com sucesso')
 
 
 def mudoucombobox():
@@ -373,6 +216,9 @@ def pesquisar():
         return 'a'
     else:
         pass
+    x = QtWidgets.QMessageBox()
+    x.setIcon(QtWidgets.QMessageBox.Question)
+    x.exec_()
 
     validacao = Funções.validacao_cdb(cdb)
     if validacao:
@@ -482,13 +328,14 @@ pro = uic2.loadUi('app.ui')
 # Isso são valores padrões
 pro.cdb_add.setValidator(QIntValidator())
 pro.cdb.setValidator(QIntValidator())
-pro.valor.setValidator(QIntValidator())
+pro.valor.setValidator(QDoubleValidator())
 pro.tipotamanho.currentIndexChanged.connect(mudoucombobox)
 pro.tamanho6.hide()
 pro.xtamanho6.hide()
 pro.atualizar.hide()
 pro.cadastrar.hide()
 # pro.comboBox.setCurrentIndex(1)
+
 # Colunas
 pro.verhistoricotabela.setColumnWidth(0, 100)
 pro.verhistoricotabela.setColumnWidth(1, 100)
@@ -505,6 +352,7 @@ pro.editar.clicked.connect(editar)
 pro.cadastrarnovo.clicked.connect(novocadastro)
 pro.cadastrar.clicked.connect(cadastrar)
 pro.atualizar.clicked.connect(atualizar)
+pro.excluir.clicked.connect(excluir)
 
 # Icones
 pro.pesquisar.setIcon(QtGui.QIcon('icons/dsds.png'))
